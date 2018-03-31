9c15982bec7fd8c47ff527dfe4a4622f52231478
Alex Sch <xantax@ukr.net>
2018-03-31 16:32:01 +0200

    []int support (#17)

--- types.go
+++ types.go
@@ -1,8 +1,42 @@
 package config
 
 import (
+	"strconv"
 	"strings"
 )
+
+// arrayInt Value implements flag.Value, flag.Getter interfaces.
+type arrayInt []int
+
+func newArrayInt(val []int, p *[]int) *arrayInt {
+	*p = val
+	return (*arrayInt)(p)
+}
+
+func (i *arrayInt) Set(val string) error {
+	*i = []int{}
+	arrStr := strings.Split(val, comma)
+	for _, currStr := range arrStr {
+		currInt, err := strconv.Atoi(currStr)
+		if err != nil {
+			return errCantUse(val, []int{})
+		}
+		*i = append(*i, currInt)
+	}
+	return nil
+}
+
+func (i *arrayInt) Get() interface{} {
+	return []int(*i)
+}
+
+func (i *arrayInt) String() string {
+	var arrStr []string
+	for _, curr := range *i {
+		arrStr = append(arrStr, strconv.Itoa(curr))
+	}
+	return strings.Join(arrStr, comma)
+}
 
 // arrayString Value implements flag.Value, flag.Getter interfaces.
 type arrayString []string
