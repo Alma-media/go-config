e6afd27e92215480804c7ac941cd953a17f61ada
Alexander Shcherbyna <xantax@ukr.net>
2018-04-06 09:33:55 +0200

    type tests

--- types_test.go
+++ types_test.go
@@ -39,6 +39,38 @@ func Test_arrayInt(t *testing.T) {
 	})
 }
 
+func Test_arrayInt64(t *testing.T) {
+	Convey("test arrayInt64 type", t, func() {
+		ptr := new([]int64)
+		val := []int64{-1, 2, -3, 4, -5, 6, -7, 8, -9, 0}
+		arrInt64 := newArrayInt64(val, ptr)
+
+		Convey("instantiate arrayInt64 with constructor func", func() {
+			So(arrInt64, ShouldHaveSameTypeAs, new(arrayInt64))
+			So(arrInt64, ShouldImplement, (*flag.Value)(nil))
+			So(arrInt64, ShouldImplement, (*flag.Getter)(nil))
+		})
+
+		Convey("get actual []int64 value from arrayInt64", func() {
+			So(arrInt64.Get(), ShouldResemble, val)
+		})
+
+		Convey("parse new arrayInt64 value from a string", func() {
+			out := []int64{0, -9, 8, -7, 6, -5, 4, -3, 2, -1}
+			arrInt64.Set("0,-9,8,-7,6,-5,4,-3,2,-1")
+			So([]int64(*arrInt64), ShouldResemble, out)
+		})
+
+		Convey("try to parse invalid arrayInt64 from a string", func() {
+			So(arrInt64.Set("false,42"), ShouldResemble, errCantUse("false,42", []int64{}))
+		})
+
+		Convey("convert arrayInt64 to a string", func() {
+			So(arrInt64.String(), ShouldEqual, "-1,2,-3,4,-5,6,-7,8,-9,0")
+		})
+	})
+}
+
 func Test_arrayString(t *testing.T) {
 	Convey("test arrayString type", t, func() {
 		ptr := new([]string)
