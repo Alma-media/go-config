f59505442a46fa91b68fb895f0ada0a90b03ab47
Alexander Shcherbyna <xantax@ukr.net>
2018-04-06 09:30:29 +0200

    []int64 proto

--- flagset.go
+++ flagset.go
@@ -25,6 +25,18 @@ func ArrayIntVar(p *[]int, name string, value []int, usage string) {
 	flag.CommandLine.Var(newArrayInt(value, p), name, usage)
 }
 
+// ArrayInt64Var defines an []int64 flag with specified name, default value, and usage string.
+// The argument p points to an []int64 variable in which to store the value of the flag.
+func (f *FlagSet) ArrayInt64Var(p *[]int64, name string, value []int64, usage string) {
+	f.Var(newArrayInt64(value, p), name, usage)
+}
+
+// ArrayInt64Var defines an []int64 flag with specified name, default value, and usage string.
+// The argument p points to an []int64 variable in which to store the value of the flag.
+func ArrayInt64Var(p *[]int64, name string, value []int64, usage string) {
+	flag.CommandLine.Var(newArrayInt64(value, p), name, usage)
+}
+
 // ArrayStringVar defines an []string flag with specified name, default value, and usage string.
 // The argument p points to an []string variable in which to store the value of the flag.
 func (f *FlagSet) ArrayStringVar(p *[]string, name string, value []string, usage string) {
